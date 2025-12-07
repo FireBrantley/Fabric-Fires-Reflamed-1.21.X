ae2a5c73981203db1a2634151ae3e2f0abe245b1
Fire <knightarc18@gmail.com>
2025-12-07 14:30:55 -0500

    Initial commit

---
+++ src/main/java/io/github/firebrantley/firesreflamed/FiresReflamedClient.java
@@ -0,0 +1,10 @@
+package io.github.firebrantley.firesreflamed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FiresReflamedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
